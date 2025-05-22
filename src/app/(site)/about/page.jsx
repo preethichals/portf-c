5953f89bfd880c3f154e70bd0613b135bdbef4ff
preethichals <108888412+preethichals@users.noreply.github.com>
2025-05-22 15:12:43 +0530

    Update page.jsx

--- src/app/(site)/about/page.jsx
+++ src/app/(site)/about/page.jsx
@@ -31,19 +31,7 @@ function About() {
           I Enjoy the process of taking an Idea and Transforming it into a
           Beautiful and Functional website.
         </p>
-
-        <div className="flex justify-center my-2">
-          <button
-            className=" text-lg rounded-md px-4 py-2 bg-gradient-to-r to-amber-600 from-red-700 
-          m-1 uppercase text-white font-medium scale-100 hover:scale-110 transition scroll-smooth"
-            style={{ letterSpacing: "0.2rem" }}
-          >
-            {" "}
-            <Link href="https://drive.google.com/file/d/1WSMqAGHHY9RLZ8-_P7L5np9nD4yjy2yQ/view?usp=sharing" target="_blank" rel="noopener noreferrer" className=" scroll-smooth">
-              View Resume
-            </Link>{" "}
-          </button>
-        </div>
+        
         <svg className="arrows">
           <path className="a1" d="M0 0 L30 32 L60 0"></path>
           <path className="a2" d="M0 20 L30 52 L60 20"></path>
